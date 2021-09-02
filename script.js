9a1aca6485d3c2539337bd735687443ae251655c
Giorgio Andrei <cipuandrei@libero.it>
2021-09-02 15:11:14 +0200

    axios-template-created

--- script.js
+++ script.js
@@ -4,6 +4,19 @@ Vue.config.devtools = true;
 
 const app = new Vue({
   el: '#app',
-  data: {},
-  methods: {},
+  data: {
+    response: [],
+  },
+  computed: {
+    ei: this.axiosTemplate(),
+  },
+  methods: {
+    axiosTemplate() {
+      axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+        .then((res) => {
+          const response = res.data.response;
+          this.response.push(response);
+        });
+    }
+  },
 });
