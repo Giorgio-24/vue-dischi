ba66564e18ffa73c0e4e9ecf5267e12fc71971fa
Giorgio Andrei <cipuandrei@libero.it>
2021-09-02 22:45:02 +0200

    order-albums-by-year

--- script.js
+++ script.js
@@ -6,18 +6,36 @@ const app = new Vue({
   el: '#app',
   data: {
     response: [],
+    getYear: [],
+    orderedResponse: [],
+  },
+  computed: {
   },
   methods: {
+    orderAlbumsByYear() {
+      this.response.forEach(element => {
+        this.orderedResponse.push(element)
+        this.orderedResponse.sort(function (a, b) { return a.year - b.year });
+      });
+    },
+
+
+
     axiosTemplate() {
       axios.get('https://flynn.boolean.careers/exercises/api/array/music')
         .then((res) => {
           const response = res.data.response;
           console.log(response);
           this.response = response;
+          this.orderAlbumsByYear();
         });
     }
   },
   created() {
     this.axiosTemplate();
+
+  },
+  mounted() {
+
   },
 });
